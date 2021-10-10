217ce9e78dac4a3b49a72248cd1bbe6d02b59db7
Samet <sam.kem.2000@gmail.com>
2021-10-10 11:27:46 +0300

    done

--- alıştırma/Program.cs
+++ alıştırma/Program.cs
@@ -6,7 +6,24 @@ namespace alıştırma
     {
         static void Main(string[] args)
         {
-            
+            int b=5;
+            decimal a=b;
+            try
+            {   Console.WriteLine("sayı giriniz");
+               //  int c=int.Parse(Console.ReadLine());
+               //  Console.WriteLine("girmiş olduğunuz sayı"+c);
+               int time=DateTime.Now.Hour;
+               string r=time>6 && time<12?"günaydın" : time>13?"öğleden sonra":"öğlen gibi";
+               Console.WriteLine(r);
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+            finally{
+                Console.WriteLine("oley");
+            }
         }
     }
 }
